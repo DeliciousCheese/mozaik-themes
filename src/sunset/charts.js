cf691af1f53d4d1bf32f7bc1a9ff0df5d6992052
Jake Byrd <byrdjake126@gmail.com>
2019-06-07 11:56:50 -0500

    sunset theme

---
+++ src/sunset/charts.js
@@ -0,0 +1,16 @@
+import colors from './colors'
+
+
+export default {
+    axis: {
+        textColor: colors.textMute,
+        fontSize: '1.3vmin',
+        tickColor: colors.textMute,
+        legendColor: colors.textMute,
+        legendFontSize: '1.3vmin',
+    },
+    grid: {
+        stroke:          '#9b645e',
+        strokeDasharray: '1,3',
+    },
+}
---
+++ src/sunset/colors.js
@@ -0,0 +1,10 @@
+export default {
+    background: '#652674',
+    text:       '#db9436',
+    textMute:   '#db641e',
+    icon:       'c86e19',
+    unknown:    '#db9436',
+    success:    '#4ec2b4',
+    warning:    '#d1be65',
+    failure:    '#a31c12',
+}
